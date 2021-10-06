e94daddf7731bcea85344d80e059e8ea1af2f38e
MatiasPJoo19 <58026139+MatiasPJoo19@users.noreply.github.com>
2021-10-06 15:08:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const { v4: uuidv4 } = require('uuid'); // uuid implementation
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');  //bodyParser implementation
+const Ajv = require('ajv');
+const ajv = new Ajv();
+const port = 3000       //local 3000 port
+
+const ItemsInfoSchema = require('./schemas/Items.schema.json')
+const ItemInfoValidator = ajv.compile(ItemsInfoSchema);
+
+app.use(bodyParser.json());
+
+const ItemInfoValidateMw = function(req, res, next) {
+
+    const validationResult = ItemInfoValidator(req.body);
+    if(validationResult == true) {
+        next();
+    }else{
+        res.sendStatus(400);
+    }
+}
+
+const Items = [
+{  
+            "id": uuidv4(),
+            "sellerName": "Tatu ",
+            "title": "used winter jacket",
+            "description": "Red winter jacket size M",
+            "city": "Oulu",
+            "photo": 0,
+            "priceRequest": 50,
+            "releaseDate": "4.10.2021",
+            "phoNumber": "+35848598735",
+            "category": "clothes",
+            "shipping": false
+    }
+ 
+];
+
+app.get('/items', (req, res) => {
+    
+        res.json(Items);
+})
+
+app.post('/items', ItemInfoValidateMw, (req, res) => {
+
+    const validationResult = ItemInfoValidator(req.body);
+    console.log(validationResult);
+if(validationResult == true) {
+    Items.push({
+    id: uuidv4(),
+    sellerName: req.body.sellerName,
+    title: req.body.title,
+    description: req.body.description,
+    city: req.body.city,
+    photo: req.body.photo,
+    priceRequest: req.body.priceRequest,
+    releaseDate: req.body.releaseDate,
+    phoNumber: req.body.phoNumber,
+    category: req.body.category,
+    shipping: req.body.shipping
+    
+
+});
+res.sendStatus(201);
+
+}})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
